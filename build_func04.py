e679f7d24113698646777cd4b6741a5ff7af8246
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2022-09-28 15:41:41 +0500

    main4

--- build_func04.py
+++ build_func04.py
@@ -1,5 +1,4 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func04
-n=3.5
-answer=3*pow((n+1),2)
-print(answer)
+n=4
+answer=pow(((2+n)/3),)
